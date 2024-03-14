8f761e0fa3858af8a5b8f1b822eaf921c2ce2081
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-03-14 22:14:17 +0200

    Update 2.ts

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,7 +11,10 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+function compare<T extends AllType, B extends AllType>(
+  top: Pick<T, "name" | "color">,
+  bottom: Pick<B, "position" | "weight">
+): AllType {
   return {
     name: top.name,
     color: top.color,
