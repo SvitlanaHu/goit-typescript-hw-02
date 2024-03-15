efd203021edaa3a5bc8ee6e563fddfd9319f803a
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-03-15 13:08:57 +0200

    Update 7.ts

--- src/generic/7.ts
+++ src/generic/7.ts
@@ -10,8 +10,7 @@ export enum UserRole {
 }
 
 // Замініть наступний код на версію за допомогою Record
-type RoleDescription = Record<UserRole, string>
-const RoleDescription = {
+const RoleDescription: Record<UserRole, string> = {
   admin: 'Admin User',
   editor: 'Editor User',
   guest: 'Guest User',
